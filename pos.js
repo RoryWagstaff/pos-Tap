2466b281f68561a1df897bfe8f9de23b2a5969f5
RoryWagstaff <32767818+RoryWagstaff@users.noreply.github.com>
2018-02-09 10:05:33 +0000

    Initial commit

---
+++ pos.js
@@ -0,0 +1,65 @@
+ const counter = document.querySelector("#counter");
+const meditationScreenInner = document.querySelector(".meditation-screen-inner");
+const container = document.querySelector(".container");
+const options = document.querySelector(".options");
+const start = document.querySelector("#start");
+const completed = document.querySelector(".completed");
+const completedTimer = document.querySelector("#completed-timer");
+const completedCounter = document.querySelector("#completed-counter");
+const input = document.querySelector("input");
+const timerId = document.querySelector("#timerid");
+
+/*
+
+const ping = new Audio("ping.wav");
+
+function pingSound () {
+	ping.play();	
+}
+
+
+*
+
+let counter1 = 0;
+
+meditationScreenInner.addEventListener("dblclick",function(){
+
+
+	counter.innerHTML = counter1;
+	counter1++;
+	if (counter1 >1) {
+		counter.style.color = "orange";
+	}	
+
+	completedCounter.innerHTML = counter1;
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+	
+
+
+
+
+
+
+	
+
+
+	
+	
+
+
